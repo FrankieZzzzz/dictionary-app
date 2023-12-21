43a2eb33e626f9601d5a88613f4968d24db1e67b
FrankieZzzz <zhongweini0703@gmail.com>
2023-12-20 20:50:44 -0500

    connect to dictionary api
    https://dictionaryapi.dev/

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyValue, setKeyValue] = useState(null);
@@ -6,6 +7,12 @@ export default function Dictionary(){
     function Searching(event){
         event.preventDefault()
         alert(`User searching for ${keyValue}`)
+
+        let dictionaryAPI = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyValue}`;
+        axios.get(dictionaryAPI).then(dictionaryFunction)
+    }
+    function dictionaryFunction(response){
+        console.log(response.data[0]);
     }
     function getSearchValue(event){
         event.preventDefault();
@@ -16,29 +23,29 @@ export default function Dictionary(){
             <p className="app-title"><span>SIMPLE</span> DICTIONARY</p>
             <form className="app-search-engine" onSubmit={Searching}>
                 <div className="app-search-engine-left">
-                    <i class="fa-solid fa-bars app-menu-icon"></i>
+                    <i className="fa-solid fa-bars app-menu-icon"></i>
                     <input type="search" autoFocus={true} className="app-search-bar" placeholder="Search" onChange={getSearchValue}/>
                 </div>
-                <button className="app-search-button" type="submit"><i class="fa-solid fa-magnifying-glass app-menu-icon" onClick={Searching}></i></button>
+                <button className="app-search-button" type="submit"><i className="fa-solid fa-magnifying-glass app-menu-icon" onClick={Searching}></i></button>
             </form>
             <section id="recent-search-section">
                 <h2>Recent Search</h2>
                 <ul className="list-unstyled recent-search-list">
-                    <li class="recent-search-word">
+                    <li className="recent-search-word">
                         <div>Sunny</div>
-                        <i class="fa-regular fa-heart"></i>
+                        <i className="fa-regular fa-heart"></i>
                     </li>
                     <li class="recent-search-word">
                         <div>Lucky</div>
-                        <i class="fa-regular fa-heart"></i>
+                        <i className="fa-regular fa-heart"></i>
                     </li>
                     <li class="recent-search-word">
                         <div>Tomorrow</div>
-                        <i class="fa-regular fa-heart"></i>
+                        <i className="fa-regular fa-heart"></i>
                     </li>
-                    <li class="recent-search-word">
+                    <li className="recent-search-word">
                         <div>Swimming pool</div>
-                        <i class="fa-regular fa-heart"></i>
+                        <i className="fa-regular fa-heart"></i>
                     </li>
                 </ul>
             </section>
